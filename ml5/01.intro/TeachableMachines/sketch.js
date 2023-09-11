d5af78f7c2837439dbabec87a002566def84b878
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-09-11 17:53:22 +0100

    ml5 | teachable machines

--- ml5/01.intro/TeachableMachines/sketch.js
+++ ml5/01.intro/TeachableMachines/sketch.js
@@ -1,33 +1,39 @@
 /*
   Data and machine learning for artistic practice (DMLAP)
-  Week 3
+  Week 1
   
-  Image classifier via model trained with Teachable Machines
+  Image classifier via models trained with Teachable Machines
   adapted from https://github.com/ml5js/Intro-ML-Arts-IMA-F21
+  & Joe McAlister's course
 */
-
 
 // Classifier Variable
 let classifier;
+
 // Model URL (replace with your model trained on teachablemachine.withgoogle.com)
-//const imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/'; // Night vs Day
-const imageModelURL = 'https://teachablemachine.withgoogle.com/models/pcs6MFoPB/'; // Simley vs Star (drawings)
+// const imageModelURL = 'https://teachablemachine.withgoogle.com/models/pcs6MFoPB/'; // Simley vs Star (drawings)
+// const imageModelURL = 'https://teachablemachine.withgoogle.com/models/H7dm-bnTn/'; // Four faces: normal, flipped left, flipped right, upside down
+const imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';  // Night vs Day (put your hand in front of the webcam)
 
 // Video
 let video;
 let flippedVideo;
+
 // To store the classification
 let label = "";
 let confidence = 0.0;
 
 // Load the model first
 function preload() {
-  // eslint-disable-next-line prefer-template
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json'); // IDEA: try instantiate *two* models, instead of one? You could train a model associating
+                                                                  // postures with poetic snippets (the class names), and display them on the screen? Note that
+                                                                  // you would need to change the classifyVideo function, which currently uses the classifier
+                                                                  // global variable by default.
 }
 
 function setup() {
   createCanvas(320, 260);
+
   // Create the video
   video = createCapture(VIDEO);
   video.size(320, 240);
@@ -47,7 +53,10 @@ function draw() {
   fill(255);
   textSize(16);
   textAlign(CENTER);
-  text(label + ' confidence:' + confidence.toFixed(2), width / 2, height - 4);
+
+  if (label) { // don't display if the label is empty
+    text(` ${label} (${confidence.toFixed(2) * 100}% confidence)`, width / 2, height - 4);
+  }
 }
 
 // Get a prediction for the current video frame
@@ -65,8 +74,11 @@ function gotResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  label = results[0].label;
-  confidence = results[0].confidence;
+  label = results[0].label;           // IDEA: can you modify this sketch to display not just the first
+  confidence = results[0].confidence; // label (results[0]), but all of them? You would need to change 
+                                      // the code that displays the label as a text in `draw()`. One way
+                                      // of doing this would be to use `results` as a global!
+
   // Classifiy again!
   classifyVideo();
 }
