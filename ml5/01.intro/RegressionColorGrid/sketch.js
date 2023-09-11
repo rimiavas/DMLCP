c06ed978a25659b7f5dc139d9acfc013f14808a1
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-09-11 20:38:03 +0100

    ml5 | intro, RegressionColorGrid

--- ml5/01.intro/RegressionColorGrid/sketch.js
+++ ml5/01.intro/RegressionColorGrid/sketch.js
@@ -1,78 +1,86 @@
 /*
 
-In this code we create colour markers on the screen, each time saving their coordinates and r,g,b values and providing them to our neural network.
+In this code we create colour markers on the screen, each time saving their
+coordinates and r,g,b values and providing them to our neural network.
 e.g. nnAddData(inputs, output); in mousePressed();
 
 Once we have enough points we call nnTrain(); this trains the neural network
 
-The demo will show a grid with all the colors predicted at each position
-
-This code demo drastically simplifies the process of training a network as it's week 1 - but later in the module we will do this for ourselves!
+The demo will show a grid with all the colors predicted at each position.
 
 Instructions:
 - Click to place a point,
-- Press 1 to 4 to change the colour of the points
+- Press 1 to 3 to change the colour of the points
 - Press 't' to train the model
+
+All information to customise your neural network and training can be found here:
+https://learn.ml5js.org/#/reference/neural-network
+
+IDEA:
+- Can you find patterns where the neural net fails to capture the data?
+- Maybe you would like to develop this sketch to allow you to explore the different parameters
+  of your network and of training more easily? One way of doing that would be to work with a
+  GUI library like https://github.com/bitcraftlab/p5.gui
+- In this vein, one could imagine implementing ways to:
+  - Show which class is currently selected (1, 2 or 3) somewhere on the canvas
+  - Reset the neural network when pressing a key
+  - Reset the datapoints when pressing another key
+  - Show the prediction for the pixel under the mouse (draw an ellipse with the predicted color?)
+  - If you wanted to save/load your data points, here's how to do it:
+
+    // This will actually download a file.
+    function saveData(path){
+      saveJSON(data_points, './data.json');
+    }
+
+    // Loading and saving
+    function loadData(path){
+      function loaded(json){
+        // Note: you might want to recreate your nn here (or not)
+        data_points = json;              // Load existing data if any
+        for (const data of data_points){ // if we have points add them
+          let inputs = [data.x, data.y];
+          let outputs = [data.r, data.g, data.b];
+          nn.addData(inputs, outputs); // this adds the data to the neural network
+        }
+      }
+      loadJSON(path, loaded);
+    }
 
 */
 
+let nn,
+    mode = "training";
+
 let data_points = [];
 
-function initNeuralNetwork() {
-  // setup the neural network (see brain.js)
-  // for each example, the network has two inputs [x, y] (the mouse position) and three outputs [r, g, b] (the corresponding color)
-  nnSetup({inputs:2,   // do not change this
-           outputs: 3, // do not change this
-           hiddenUnits: 16, // the number of hidden units in our neural network (generally this should be proportional to the dataset size)
-           architecture: 'default', // try changing this to 'tanh' for a different neural network architecture
-           });
-}
+let canvasW = 500,
+    canvasH = 500;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(canvasW, canvasH);
 
   // set our background once at the start
   background(255);
 
   ourColor = color(255,0,0);
 
-  initNeuralNetwork();
-
-  // Uncomment to load an existing configuration of colors
-  // loadData('data.json');
+  // Setup the neural network
+  // For each example, the network has two inputs [x, y] (the mouse position) and three outputs [r, g, b] (the corresponding color)
+  // (Here we use the default config. To add more, look here under 'regression': https://learn.ml5js.org/#/reference/neural-network?id=defining-custom-layers)
+  nn = ml5.neuralNetwork({
+    inputs: 2,          // two inputs: x and y
+    outputs: 3,         // three outputs: r, g and b
+    task: 'regression', // because we predict the three numbers directly (r/g/b values)
+    debug: true         // this opens the training pane
+  });
 }
 
-
-// This function draws a grid predicting the color for each cell in the grid
-function drawGrid() {
-  let step = 20; // The coarsness of the grid
-  // Collect center points of each cell and put the points in an array
-  // the array will be ordered by rows of the grid
-  let inputs = [];
-  for (let y = 0; y < 500; y += step){
-    for (let x = 0; x < 500; x += step){
-      inputs.push([(x + step/2)/500, (y + step/2)/500]);
-    }
-  }
-
-  // Predict colors for ALL collected center points
-  let res = nnPredict(inputs);
-
-  // Now draw the grid with the predicted colors
-  let i = 0;
-  for (let y = 0; y < 500; y += step){
-    for (let x = 0; x < 500; x += step){
-      const clr = res[i];
-      fill(clr[0], clr[1], clr[2]);
-      rect(x, y, step, step);
-      i += 1; // Increment cell (recall these are ordered by row and the inner loop goes along a row)
-    }
-  }
-}
 
 function draw() {
-  // note in this demo we update the canvas every frame
+  // Note in this demo we update the canvas every frame
   background(200);
+
   fill(0);
   noStroke();
   text("Click to place a point\nPress 1-3 to change color\nPress 't' to train.", 20, 20);
@@ -81,11 +89,12 @@ function draw() {
   stroke(1);
   ellipse(mouseX, mouseY, 5, 5);
 
-  // if we have already trained the neural network draw a grid with the predicted colors
+  // Draw the grid
    if (mode === "demo") {
      drawGrid();
    }
 
+  // Draw our data points
   for (const p of data_points){
     fill(p.r, p.g, p.b);
     stroke(0);
@@ -94,28 +103,23 @@ function draw() {
 }
 
 function mousePressed() {
+  // IDEA: perhaps we want to modify the sketch so that it is possible to
+  // add more data after training, and train again?
   if (mode == "training") {
-    // draw a circle at our mouse coordinates, set to the colour of our current color.
-    let inputs = [mouseX/500, mouseY/500];
-	  let output = [red(ourColor),
-				          green(ourColor),
-				          blue(ourColor)];
-
-    data_points.push({x: mouseX,
-                      y: mouseY,
-                      r: red(ourColor),
-                      g: green(ourColor),
-                      b: blue(ourColor)});
-
-    nnAddData(inputs, output);
+    // Draw a circle at our mouse coordinates, set to the colour of our current color.
+    // let inputs = [mouseX/canvasW, mouseY/canvasH];
+    let inputs = [mouseX, mouseY];
+    let outputs = [red(ourColor), green(ourColor), blue(ourColor)];
+    data_points.push({x: mouseX, y: mouseY,
+                      r: red(ourColor), g: green(ourColor), b: blue(ourColor)});
+    nn.addData(inputs, outputs);
   }
 }
 
 function keyPressed() {
   // Switch is equivalent to a series of if-else statements
-  //
   switch (key) {
-    // 1, 2, 3 select colors
+    // 1, 2, 3 select colors              // IDEA: add more colors?
     case "1":
       ourColor = color(255,0,0);
       break; // Important to call break after each "case" otherwise execution will continue to the next
@@ -127,40 +131,81 @@ function keyPressed() {
       break;
     // "t" starts training the neural network! (see brain.js)
     case "t":
-      nnTrain(50, {liveUpdate:true, // if true, we will see the result at each iteration of training
-                    learningRate: 0.2, // "speed" at which the training procedure tries to reach the minimum error
-                                       // should be less than 1
-                 });
-      break;
-    case "l":
-      loadData("data.json");
-      break;
-    case "s":
-      saveData("data.json");
+      // WHERE THE TRAINING HAPPENS: no need to change this
+      // More information here: https://learn.ml5js.org/#/reference/neural-network?id=train
+      nn.normalizeData();    // Normalise our data
+      nn.train({
+        epochs: 35,          // This controls for how long we train!
+        batchSize: 32,       // We will see what all this means soon
+        learningRate: 0.2,
+        validationSplit: 0.0 // By default we won't perform any validation (this does not influence training, but removes datapoints if > 0)
+      }, finishedTraining);  // This is a callback: the function finishedTraining is called when the training is over
+      mode = "demo";         // This signals that we want to see updates as we train (might slow down training)
       break;
   }
 }
 
-// Loading and saving
-function loadData(path){
-  function loaded(json){
-    // Clear the neural network
-    initNeuralNetwork();
-    // Load existing data if any
-    data_points = json;
-    // if we have points add them
-    for (const data of data_points){
-      let input = [data.x, data.y];
-      let output = [data.r, data.g, data.b];
-      // this adds the data to the neural network
-      nnAddData(input, output);
+// Draw the grid
+function drawGrid() {
+  let step = 20; // The square size for the grid
+
+  // Collect center points of each cell and put the points in an array
+  // (the array will be ordered by rows of the grid)
+  let inputs = [];
+  for (let y = 0; y < canvasW; y += step){
+    for (let x = 0; x < canvasH; x += step){
+      inputs.push([(x + step/2), (y + step/2)]);
     }
   }
 
-  loadJSON(path, loaded);
+  // Predict colors for ALL collected center points
+  let res = nnPredict(inputs);
+
+  // Now draw the grid with the predicted colors
+  let i = 0;
+  for (let y = 0; y < canvasW; y += step){
+    for (let x = 0; x < canvasH; x += step){
+      const clr = res[i];
+      fill(clr[0], clr[1], clr[2]);
+      rect(x, y, step, step);
+      i += 1; // Increment cell (recall these are ordered by row and the inner loop
+    }         // goes along a row)
+  }
 }
 
-// This will actually download a file. 
-function saveData(path){
-  saveJSON(data_points, './data.json');
+// NN things
+function finishedTraining(){
+  console.log("We finished training!");
+  console.log("Here's how one raw prediction looks like for one datapoint:");
+  console.log(nn.predictSync([width/2,height/2])); // prediction for the centre of the canvas
+}
+
+function nnPredict(input) {
+
+  // The standard ML5js way to perform predictions is asyncronous, but async
+  // programming can be tricky. Luckily the "predictSync" function forces ML5js
+  // to give us the result immediately and simplifies matters
+  const res = nn.predictSync(input);
+
+  // ML5 places each component of the prediction into an object.
+  // As a result, the format of the output of a prediction is different from
+  // the one given as training data. E.g. if we trained the network with one
+  // input array [x] and an output array [a, b, c] one prediction will give
+  // [{0:predicted_a, label: 0, value:predicted_a, unNormalizedValue: unnormalized_predicted_a},
+  //  {1:predicted_b, label: 1, value:predicted_b, unNormalizedValue: unnormalized_predicted_b},
+  //  {2:predicted_c, label: 2, value:predicted_c, unNormalizedValue: unnormalized_predicted_c}]
+  // meaning that with "straight" ML5js we will need to parse these values and
+  // put them into our preferred format (which is likely the one we provided in
+  // the first place as training data).
+
+  // If we provide an array of inputs (asking for multiple predictions),
+  // predictedSync will return an array of such objects, so here we handle the
+  // two cases making it consistent with the input format
+  if (Array.isArray(input[0])){
+    // multiple predictions
+    return res.map(element => element.map(v => v.value));
+  }else{
+    // make one prediction
+    return res.map(v => v.value);
+  }
 }
