3cb3702f284ea2a5f7ca05df072a7f78236d1861
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-09-11 23:05:53 +0100

    ml5 | intro. RegressionColorBasic

--- ml5/01.intro/RegressionColorBasic/sketch.js
+++ ml5/01.intro/RegressionColorBasic/sketch.js
@@ -1,40 +1,62 @@
 /*
 
-In this code we create colour markers on the screen, each time saving their coordinates and r,g,b values and providing them to our neural network.
-e.g. nnAddData(inputs, output); in mousePressed();
+In this code we create colour markers on the screen, each time saving their
+coordinates and r,g,b values and providing them to our neural network.
+e.g. nn.addData(inputs, output); in mousePressed();
 
-Once we have enough points we call nnTrain(); this trains the neural network
+Once we have enough points we call nn.train(); this trains the neural network
 
-After it has trained we ask the neural network to predict the colour for the mouse coordinates (reflected in the square)
-
-This code demo drastically simplifies the process of training a network as it's week 1 - but later in the module we will do this for ourselves!
+After it has trained we ask the neural network to predict the colour for the
+mouse coordinates (reflected in the square)
 
 Instructions:
 - Click to place a point,
-- Press 1 to 4 to change the colour of the points
-- Press 'T' to train the model
+- Press 1 to 3 to change the colour of the points
+- Press 't' to train the model
 
+IDEA: An experiment that could help you gain some practice with this NN system
+      could be to try and implement two variants of this sketch: one that takes
+      *one* input instead of two (for instance only the x or the y). You would
+      need to make changes when you define your network, and whenever an input
+      is fed into your nn. You could also try and imagine what system could take
+      *three* inputs instead of two?
+IDEA: Similarly, nothing stops you from making the nn predict only one number,
+      instead of three. Instead of having three different colours as we have here,
+      you could define three different radius sizes for ellipses. You select which
+      one using the numbers as above, and when you click to create data, you create
+      an ellipse with that radius at that point. Then, you train your neural net to
+      predict just this number, the radius, and when the model is trained, you could
+      draw an ellipse with the predicted radius at that spot.
 */
 
+let nn,
+    mode = "training";
 
 function setup() {
   createCanvas(500, 500);
 
   // set our background once at the start
-  background(255);
+  background(0);
 
   // our first colour can be red;
   ourColor = color(255,0,0);
 
   // note in this demo we just clear the canvas at the beginning
-  background(200);
-  fill(0);
+  fill(255);
   noStroke();
   text("Click to place a point\nPress 1-3 to change color\nPress 't' to train.", 20, 20);
 
-  // setup the neural network (see brain.js)
-  // for each example, the network has two inputs [x, y] (the mouse position) and three outputs [r, g, b] (the corresponding color)
-  nnSetup({inputs:2, outputs:3});
+  // Setup the neural network
+  // For each example, the network has two inputs [x, y] (the mouse position)
+  // and three outputs [r, g, b] (the corresponding color)
+  // (Here we use the default config. To add more, look here under 'regression':
+  // https://learn.ml5js.org/#/reference/neural-network?id=defining-custom-layers)
+  nn = ml5.neuralNetwork({
+    inputs: 2,          // two inputs: x and y
+    outputs: 3,         // three outputs: r, g and b
+    task: 'regression', // because we predict the three numbers directly (r/g/b values)
+    debug: true         // this opens the training pane
+  });
 
 }
 
@@ -42,10 +64,17 @@ function draw() {
   // if we have already trained we want to show the output as a square
   // in the bottom right-hand side
    if (mode === "demo") {
-	   const mouseColor = nnPredict([mouseX, mouseY]);
-       print(mouseColor);
-	   fill(mouseColor[0], mouseColor[1], mouseColor[2]);
-	   rect(0, height-50, 50, 50);
+     const mouseColor = nnPredict([mouseX, mouseY]);
+
+     // IDEA: why not use the colour for the background, or a frame around the canvas?
+     // IDEA: how about changing this piece of code so that an ellipse is
+     //       drawn where the mouse is, with the color displayed accordingly?
+     // IDEA: you could also imagine looking into coding a bouncing ball (or
+     //       several), and use the position to determine the colour...
+     //       (of course, in both these cases you might want to hide the data points)
+     // console.log('mouse color:', mouseColor);
+     fill(mouseColor[0], mouseColor[1], mouseColor[2]);
+     rect(0, height-50, 50, 50);
    }
 }
 
@@ -57,24 +86,68 @@ function mousePressed() {
     ellipse(mouseX, mouseY, 10, 10);
 
     let inputs = [mouseX, mouseY];
-	let output = [red(ourColor),
-				  green(ourColor),
-				  blue(ourColor)];
+    let outputs = [
+      red(ourColor),
+      green(ourColor),
+      blue(ourColor)
+    ];
 
     // this adds the data to the neural network
-    nnAddData(inputs, output);
+    nn.addData(inputs, outputs);
   }
 }
 
 function keyPressed() {
+  // IDEA: play with more colours?
   if (key == "1") {
     ourColor = color(255,0,0);
-  }else if (key == "2") {
+  } else if (key == "2") {
     ourColor = color(0,255,0);
-  }else if (key == "3") {
+  } else if (key == "3") {
     ourColor = color(0,0,255);
-  }else if (key == "t") {
-    // this means we should train our neural network!
-    nnTrain();
+  } else if (key == "t") {
+    // WHERE THE TRAINING HAPPENS: no need to change this
+    // More information here: https://learn.ml5js.org/#/reference/neural-network?id=train
+    nn.normalizeData();    // Normalise our data
+    nn.train({
+      epochs: 35,          // This controls for how long we train!
+      batchSize: 32,       // We will see what all this means soon
+      learningRate: 0.2,
+      validationSplit: 0.0 // By default we won't perform any validation (this does not influence training, but removes datapoints if > 0)
+    }, finishedTraining);  // This is a callback: the function finishedTraining is called when the training is over
+  }
+}
+
+function finishedTraining(){
+  console.log("We finished training!");
+  console.log("Here's how one raw prediction looks like for one datapoint:");
+  console.log(nn.predictSync([width/2,height/2])); // prediction for the centre of the canvas
+  // Switch to the demo mode - this will tell our app that we can visualize the result
+  mode = "demo";
+}
+
+function nnPredict(input) {
+  // The standard ML5js way to perform predictions is asyncronous, but async programming can be tricky.
+  // Luckily the "predictSync" function forces ML5js to give us the result immediately and simplifies matters
+  const res = nn.predictSync(input);
+
+  // ML5 places each component of the prediction into an object.
+  // as a result, the format of the output of a prediction is different from the one given as training data
+  // E.g. if we trained the network with one input array [x] and an output array [a, b, c] one prediction will give
+  // [{0:predicted_a, label: 0, value:predicted_a, unNormalizedValue: unnormalized_predicted_a},
+  //  {1:predicted_b, label: 1, value:predicted_b, unNormalizedValue: unnormalized_predicted_b},
+  //  {2:predicted_c, label: 2, value:predicted_c, unNormalizedValue: unnormalized_predicted_c}]
+  // meaning that with "straight" ML5js we will need to parse these values and put them into our preferred format
+  // (which is likely the one we provided in the first place as training data).
+  //
+  // If we provide an array of inputs (asking for multiple predictions), predictedSync will return an array of such objects, so here we handle the two cases
+
+  // making it consistent with the input format
+  if (Array.isArray(input[0])){
+    // multiple predictions
+    return res.map(element => element.map(v => v.value));
+  }else{
+    // make one prediction
+    return res.map(v => v.value);
   }
 }
