0bb69b8c66153d67e2592bde21c822a13923989b
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-09-14 22:29:05 +0100

    ml5 | 03, FacialDetectionWebcam

--- ml5/03.facial-detection-pose/FacialDetectionWebcam/sketch.js
+++ ml5/03.facial-detection-pose/FacialDetectionWebcam/sketch.js
@@ -1,153 +1,166 @@
 /*
   Data and machine learning for artistic practice
-  Week 3
-  
+  Adapted from Joe McAlister, originally from
+  here: https://learn.ml5js.org/#/reference/face-api
+
   Facial detection on a webcam
-  
 */
 
 let faceapi,
     video,
-    detections;
+    detections,
+    hasLogged = false; // to inspect our object just once, see below
 
-// these are our options for detecting faces, provided by ml5.js
+// These are our options for detecting faces, provided by ml5.js
 const detection_options = {
-    withLandmarks: true,
-    withDescriptors: false,
+  withLandmarks: true,
+  withDescriptors: false,
 }
 
 function setup() {
-  createCanvas(600, 338); // I use these to downsize a 720p stream, but you can adjust for your webcam if it skews it.
- 
-  // ask for webcam access - with webcamReady() callback for when we have access
-  video = createCapture(VIDEO, webcamReady);
-  video.size(width, height); // set size to be equal to canvas
-  video.hide(); // hide DOM element
+  createCanvas(600, 338);                    // I use these to downsize a 720p stream, but you can adjust for your webcam if it skews it.
+
+  video = createCapture(VIDEO, webcamReady); // Ask for webcam access – with the `webcamReady` callback for when we have access
+  video.size(width, height);                 // Set size to be equal to canvas
+  video.hide();                              // Hide DOM element
 }
 
 function webcamReady(stream) {
-  // load the faceapi model - with modelReady() callback
-  // - NOTE: this time we provide video as the first parameter
+  // Load the faceapi model – with a `modelReady` callback
   faceapi = ml5.faceApi(video, detection_options, modelReady)
 }
 
 function draw() {
   background(0);
-  
-  // draw picture
+
+  // Draw picture
   image(video, 0,0, width, height)
-  
-  // if we have detections, draw them on the image
+
+  // If we have detections, draw them on the image
   if (detections) {
-    // when we call detect, we are looking for potentially multiple faces, so ml5.js returns an array of objects, therefore here we use a for loop to get each 'person'.
-    for (let person of detections) {
-      drawBox(person);
-      drawLandmarks(person);
-    }
+    for (let person of detections) { // When we call detect, we are looking for potentially multiple faces, so
+      drawBox(person);               // ml5.js returns an array of objects, therefore here we use a for loop to
+      drawLandmarks(person);         // get each 'person'. Note the for...of loop:
+    }                                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
   }
+
+  // IDEA: like in previous sketches, there is no obligation to display the video,
+  //       and you could for instance imagine a blank canvas where points from the
+  //       face, or the box itself, can be used in artistic ways! To draw or
+  //       influence shapes on the canvas in real time!
+
 }
 
-
-// callback for when ml5.js has loaded the model
+// Callback for when ml5.js has loaded the model
 function modelReady() {
   console.log("Model is ready...");
 
-  // ask ml5 to detect a faces in the video stream previously provided - gotResults() callback
-  faceapi.detect(gotResults);
+  faceapi.detect(gotResults); // Ask ml5 to detect a faces in the video stream previously
+                              // provided, then call the `gotResults` callback
 }
 
 // ml5.js has determined if there's a face
 function gotResults(err, result) {
-    // check if ml5.js returned an error - if so print to console and stop
-    if (err) {
-        console.log(err)
-        return
-    }
-      
-    // if it gets here we are okay, so store results in the detections variable, this is an OBJECT of detections - see the console
-    //console.log(result);
-    detections = result;
-      
-    // we recursively call face detect
-    faceapi.detect(gotResults)
-}
-  
 
-// *** Draw our elements on the image, a box and face feature locations ***  
+  if (err) {                  // Check if ml5.js returned an error – if so print to console and stop
+    console.log(err)
+    return
+  }
+
+  detections = result;        // If it gets here we are okay, so store results in the detections variable.
+
+  if (!hasLogged) {           // Only log once!
+    console.log("The predictions object:");
+    console.log(result);      // This is an object – have a look at it in the console!
+    hasLogged = true;
+  }
+
+  faceapi.detect(gotResults); // Recursion! <3 We call face detect again
+}
+
+
+// Draw our elements on the image, a box and face feature locations
 function drawBox(detections){
-    const alignedRect = detections.alignedRect;
-    const {_x, _y, _width, _height} = alignedRect._box;
-    noFill();
-    stroke(161, 95, 251);
-    strokeWeight(2)
-    rect(_x, _y, _width, _height)
+  push(); // Precaution: make sure these changes stay in this function
+
+  noFill();
+  strokeWeight(2);
+  stroke(161, 95, 251);
+
+  const alignedRect = detections.alignedRect;
+  const {_x, _y, _width, _height} = alignedRect._box; // The _variables are the actual properties of the `_box` object!
+  rect(_x, _y, _width, _height);
+
+  pop();
 }
 
 function drawLandmarks(detections){
-    /* 
-      In this example we use forEach(), this is a quick way of looping through the objects in an array. It will in this case return each value in the array sequentially as the local variable "item".
-      
-      In this case, it's equivilent of doing this:
-      for (let i=0; i<detections.parts.mouth.length; i++) {
-        let item = detections.parts.mouth.length[i];
-        
+  /*
+    In this example we use forEach(), this is a quick way of looping through
+    the objects in an array. It will in this case return each value in the
+    array sequentially as the local variable "item".
+
+    In this case, it's equivalent of doing:
+      for (let i = 0; i < detections.parts.mouth.length; i++) {
+        let item = detections.parts.mouth[i];
+        vertex(item._x, item._y);
+      }
+
+    or:
+
+      for (let item of detections.parts.mouth) {
         //...
       }
-      
-      or 
-      
-      for (let item in detections.parts.mouth.length) {
-        //...
-      }
-    */
-  
-    noFill();
-    stroke(161, 95, 251);
-    strokeWeight(2)
-    
-    push()
-    // mouth
-    beginShape();
-    detections.parts.mouth.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape(CLOSE);
+  */
 
-    // nose
-    beginShape();
-    detections.parts.nose.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape(CLOSE);
+  push(); // Keep the styles within this function!
 
-    // left eye
-    beginShape();
-    detections.parts.leftEye.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape(CLOSE);
+  noFill();
+  strokeWeight(2);
+  stroke(161, 95, 251);
 
-    // right eye
-    beginShape();
-    detections.parts.rightEye.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape(CLOSE);
+  // mouth
+  beginShape();
+  detections.parts.mouth.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape(CLOSE);
 
-    // right eyebrow
-    beginShape();
-    detections.parts.rightEyeBrow.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape();
+  // nose
+  beginShape();
+  detections.parts.nose.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape(CLOSE);
 
-    // left eye
-    beginShape();
-    detections.parts.leftEyeBrow.forEach(item => {
-        vertex(item._x, item._y)
-    })
-    endShape();
+  // left eye
+  beginShape();
+  detections.parts.leftEye.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape(CLOSE);
 
-    pop();
+  // right eye
+  beginShape();
+  detections.parts.rightEye.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape(CLOSE);
+
+  // right eyebrow
+  beginShape();
+  detections.parts.rightEyeBrow.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape();
+
+  // left eye
+  beginShape();
+  detections.parts.leftEyeBrow.forEach(item => {
+    vertex(item._x, item._y);
+  })
+  endShape();
+
+  pop();
 
 }
